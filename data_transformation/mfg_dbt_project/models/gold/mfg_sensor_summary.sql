c3d9c4d23dad83bba693de2d2f4632efbf8c1a51
R-7git <roshansingha4@gmail.com>
2026-03-17 01:01:10 +0530

    fix: remove semic

--- data_transformation/mfg_dbt_project/models/gold/mfg_sensor_summary.sql
+++ data_transformation/mfg_dbt_project/models/gold/mfg_sensor_summary.sql
@@ -1,22 +1,11 @@
 {{ config(materialized='table') }}
 
--- 1. Reference the cleaned Silver data
-WITH silver_data AS (
-    SELECT * FROM {{ ref('silver_sensor_readings') }}
-),
-
--- 2. Aggregate logic for the Executive Dashboard
-final_metrics AS (
-    SELECT
-        sensor_id,
-        metric_type,
-        COUNT(*) AS total_readings,
-        AVG(metric_value) AS avg_reading_value,
-        MAX(metric_value) AS peak_reading,
-        -- How many times did this machine hit a 'CRITICAL' state?
-        COUNT(CASE WHEN health_status = 'CRITICAL' THEN 1 END) AS critical_event_count
-    FROM silver_data
-    GROUP BY 1, 2
-)
-
-SELECT * FROM final_metrics
+SELECT 
+    "SENSOR_ID",
+    "METRIC_NAME" as METRIC_TYPE,
+    COUNT(*) as TOTAL_READINGS,
+    AVG("METRIC_VALUE") as AVG_READING_VALUE,
+    MAX("METRIC_VALUE") as PEAK_READING_VALUE,
+    COUNT(CASE WHEN "METRIC_VALUE" > 90 THEN 1 END) as HEALTH_STATUS_COUNT
+FROM {{ ref('silver_sensor_readings') }}
+GROUP BY 1, 2
