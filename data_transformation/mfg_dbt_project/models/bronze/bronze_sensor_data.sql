6effdfdd85adc5329000e3c27fdd0015b844659e
R-7git <roshansingha4@gmail.com>
2026-03-17 01:14:20 +0530

    fix: explicit bron

--- data_transformation/mfg_dbt_project/models/bronze/bronze_sensor_data.sql
+++ data_transformation/mfg_dbt_project/models/bronze/bronze_sensor_data.sql
@@ -1,9 +1,5 @@
 {{ config(materialized='view') }}
 
--- Industry Standard: Selecting from the landing table populated by the producer
 SELECT 
-    sensor_id,
-    metric_name,
-    metric_value,
-    ingestion_timestamp
-FROM MFG_BRONZE_DB.RAW_DATA.SENSOR_DATA_LANDING
+    * 
+FROM {{ source('bronze_source', 'SENSOR_DATA_LANDING') }}
--- data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
+++ data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
@@ -1,7 +1,15 @@
+{{ config(materialized='table') }}
+
+WITH raw_data AS (
+    -- Reference the Bronze VIEW we just updated above
+    SELECT * FROM {{ ref('bronze_sensor_data') }}
+)
+
 SELECT 
     SENSOR_ID AS "SENSOR_ID",
-    -- This strips (C), (BAR), etc. to leave just 'TEMPERATURE', 'PRESSURE'
+    -- This regex strips '(C)', '(BAR)', etc. and forces UPPERCASE
     UPPER(REGEXP_REPLACE(METRIC_NAME, ' \\(.*\\)', '')) AS "METRIC_NAME",
     METRIC_VALUE AS "METRIC_VALUE",
     INGESTION_TIMESTAMP AS "INGESTION_TIMESTAMP"
 FROM raw_data
+WHERE SENSOR_ID IS NOT NULL
