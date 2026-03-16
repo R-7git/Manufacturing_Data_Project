99ae9a0832d540a46860480ec90b6582fb7d2993
R-7git <roshansingha4@gmail.com>
2026-03-17 00:59:24 +0530

    fix: remove semicol

--- data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
+++ data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
@@ -1,25 +1,13 @@
 {{ config(materialized='table') }}
 
--- 1. Reference the Bronze model (This creates the lineage)
-WITH raw_readings AS (
+WITH raw_data AS (
     SELECT * FROM {{ ref('bronze_sensor_data') }}
-),
-
--- 2. Apply "Senior Developer" Transformations
-transformed AS (
-    SELECT
-        sensor_id,
-        UPPER(metric_name) AS metric_type, -- Standardizing to Uppercase
-        metric_value,
-        -- Engineering: Converting timestamps to Snowflake standard
-        ingestion_timestamp::TIMESTAMP_NTZ AS ingested_at,
-        -- Business Logic: Adding a status flag for the factory floor
-        CASE 
-            WHEN metric_value > 100 THEN 'CRITICAL'
-            WHEN metric_value > 80 THEN 'WARNING'
-            ELSE 'STABLE'
-        END AS health_status
-    FROM raw_readings
 )
 
-SELECT * FROM transformed
+SELECT 
+    sensor_id AS "SENSOR_ID",
+    UPPER(metric_name) AS "METRIC_NAME",
+    metric_value AS "METRIC_VALUE",
+    ingestion_timestamp AS "INGESTION_TIMESTAMP"
+FROM raw_data
+WHERE sensor_id IS NOT NULL
