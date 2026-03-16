44b2e080aff65ba4bba72b09f10807e6885407a6
R-7git <roshansingha4@gmail.com>
2026-03-17 01:08:30 +0530

    fix: remove semicdsvsdnhtfghbjn

--- data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
+++ data_transformation/mfg_dbt_project/models/silver/silver_sensor_readings.sql
@@ -1,14 +1,7 @@
-{{ config(materialized='table') }}
-
-WITH raw_data AS (
-    SELECT * FROM {{ ref('bronze_sensor_data') }}
-)
-
 SELECT 
     SENSOR_ID AS "SENSOR_ID",
-    -- Industry Standard: Trim and Upper to prevent 'Accepted Values' failure
-    TRIM(UPPER(METRIC_NAME)) AS "METRIC_NAME",
+    -- This strips (C), (BAR), etc. to leave just 'TEMPERATURE', 'PRESSURE'
+    UPPER(REGEXP_REPLACE(METRIC_NAME, ' \\(.*\\)', '')) AS "METRIC_NAME",
     METRIC_VALUE AS "METRIC_VALUE",
     INGESTION_TIMESTAMP AS "INGESTION_TIMESTAMP"
 FROM raw_data
-WHERE SENSOR_ID IS NOT NULL
