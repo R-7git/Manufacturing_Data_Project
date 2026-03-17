2232bd8cf85b5090e67b97c2d0c8e07071242d6d
R-7git <roshansingha4@gmail.com>
2026-03-17 17:44:23 +0530

    vvvvv

--- data_transformation/mfg_dbt_project/macros/generate_schema_name.sql
+++ data_transformation/mfg_dbt_project/macros/generate_schema_name.sql
@@ -1,12 +1,14 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {# 
-       This macro overrides dbt's default behavior of prefixing custom schemas 
-       with the target schema name. It ensures our Medallion layers 
-       (RAW_DATA, STAGING_CLEANED, DATA_MARTS) are created exactly as named.
+       This macro overrides dbt's default behavior.
+       We add a check for 'target.schema' to ensure the IDE can resolve the path.
     #}
+    {%- set default_schema = target.schema -%}
+
     {%- if custom_schema_name is none -%}
-        {{ target.schema }}
+        {{ default_schema }}
     {%- else -%}
         {{ custom_schema_name | trim }}
     {%- endif -%}
+
 {%- endmacro %}
