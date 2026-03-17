78c53a561a4b540c8b3f0313279b9c94fbdabc27
R-7git <roshansingha4@gmail.com>
2026-03-17 20:16:11 +0530

    dfccvcb

---
+++ data_transformation/mfg_dbt_project/models/gold/mv_sensor_health_summary.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='materialized_view',
+    schema='RPT_SCHEMA',
+    database='DW_DB'
+) }}
+
+SELECT 
+    SENSOR_ID,
+    METRIC_NAME,
+    AVG(METRIC_VALUE) AS AVG_VALUE,
+    MAX(METRIC_VALUE) AS PEAK_VALUE,
+    COUNT(*) AS TOTAL_READINGS,
+    -- We use the SCD Type 1 Master table as the base
+    CURRENT_TIMESTAMP() AS LAST_REFRESHED_AT
+FROM {{ ref('rpt_sensor_master') }}
+GROUP BY 1, 2
