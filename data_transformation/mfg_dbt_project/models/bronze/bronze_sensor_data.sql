197c7a7654890ccafc638e26fcb0d92302f8aaf6
R-7git <roshansingha4@gmail.com>
2026-03-20 15:36:58 +0530

    dreiffnn

--- data_transformation/mfg_dbt_project/models/bronze/bronze_sensor_data.sql
+++ data_transformation/mfg_dbt_project/models/bronze/bronze_sensor_data.sql
@@ -1,5 +1,8 @@
 {{ config(materialized='view') }}
 
-SELECT 
-    * 
-FROM {{ source('stg_db', 'STG_SENSOR_DATA') }} -- Changed from 'bronze_source' to 'stg_db'
+SELECT
+    RECORD_CONTENT:sensor_id::STRING AS sensor_id,
+    RECORD_CONTENT:metric_name::STRING AS metric_name,
+    RECORD_CONTENT:metric_value::FLOAT AS metric_value,
+    RECORD_CONTENT:ingestion_timestamp::TIMESTAMP AS ingestion_timestamp
+FROM {{ source('bronze_layer', 'MFG_SENSOR_STREAM') }}
