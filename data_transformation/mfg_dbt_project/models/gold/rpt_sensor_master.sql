57c1c9a07d06de27bb9d3112bfdd7363d120c099
R-7git <roshansingha4@gmail.com>
2026-03-20 15:50:26 +0530

    dreiff

--- data_transformation/mfg_dbt_project/models/gold/rpt_sensor_master.sql
+++ data_transformation/mfg_dbt_project/models/gold/rpt_sensor_master.sql
@@ -1,12 +1,27 @@
-{{ config(materialized='incremental', unique_key='sensor_id') }}
+{{ config(
+    materialized='incremental',
+    unique_key='sensor_id',
+    incremental_strategy='merge'
+) }}
+
+WITH source_data AS (
+    SELECT * FROM {{ ref('bronze_sensor_data') }}
+),
+
+-- Use QUALIFY to pick only the most recent record per sensor_id
+latest_records AS (
+    SELECT * 
+    FROM source_data
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY sensor_id ORDER BY last_updated_at DESC) = 1
+)
 
 SELECT 
     sensor_id,
     metric_name,
     metric_value,
-    ingestion_timestamp AS last_updated_at
-FROM {{ ref('bronze_sensor_data') }}
+    last_updated_at
+FROM latest_records
 
 {% if is_incremental() %}
-  WHERE ingestion_timestamp > (SELECT MAX(last_updated_at) FROM {{ this }})
+  WHERE last_updated_at > (SELECT MAX(last_updated_at) FROM {{ this }})
 {% endif %}
